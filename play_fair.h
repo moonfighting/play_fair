aee4c67cfacae0613c58a93f22ff834fffe63d88
moonfighting <moonfighting@hotmail.com>
2015-12-17 23:24:46 +0800

    add comment

--- play_fair.h
+++ play_fair.h
@@ -11,9 +11,9 @@ class PlayFair
 public:
 	PlayFair():key(){}
 	PlayFair(const string & _key);
-	void set_key(const string & _key);
-	string encode(string message);
-	string decode(const string &code) const;
+	void set_key(const string & _key); //设置密钥
+	string encode(string message); // 对明文message进行加密，返回密文
+	string decode(const string &code) const; //对密文code进行解密，返回明文
 
 private:
 	void process_message(string &message, string &processed_message);
